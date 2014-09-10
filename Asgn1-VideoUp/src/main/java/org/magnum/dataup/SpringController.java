d50fba1f2a4e198b9a14b5b57e5f9d00f9431ce9
Nan <nanhao310@gmail.com>
2014-09-09 21:30:01 -0400

    Some improvement on SpringController

--- Asgn1-VideoUp/src/main/java/org/magnum/dataup/SpringController.java
+++ Asgn1-VideoUp/src/main/java/org/magnum/dataup/SpringController.java
@@ -1,66 +1,104 @@
 package org.magnum.dataup;
 
+import java.io.IOException;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.atomic.AtomicLong;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.magnum.dataup.model.Video;
+import org.magnum.dataup.model.VideoStatus;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
+import org.springframework.web.multipart.MultipartFile;
 
 @Controller
-public class SpringController{
-	
-	private static final AtomicLong currentId = new AtomicLong(0L);
+public class SpringController {
 
-    private Map<Long,Video> videos = new HashMap<Long, Video>();
-	
-	@RequestMapping(value="/video", method=RequestMethod.GET)
-	public @ResponseBody Collection<Video> getVideoList(){
+	private static final AtomicLong currentId = new AtomicLong(0L);
+	private Map<Long, Video> videos = new HashMap<Long, Video>();
+	private VideoFileManager videoDataMgr;
+
+	@RequestMapping(value = VideoSvcApi.VIDEO_SVC_PATH, method = RequestMethod.GET)
+	public @ResponseBody
+	Collection<Video> getVideoList() {
 		return videos.values();
 	}
-	
-	@RequestMapping(value="/video", method=RequestMethod.POST)
-	public @ResponseBody Video addVideo(@RequestBody Video v){
-		save(v);
+
+	@RequestMapping(value = VideoSvcApi.VIDEO_SVC_PATH, method = RequestMethod.POST)
+	public @ResponseBody
+	Video addVideo(@RequestBody Video v) {
 		v.setDataUrl(getDataUrl(v.getId()));
+		save(v);
 		return v;
 	}
+
+	@RequestMapping(value = VideoSvcApi.VIDEO_DATA_PATH, method = RequestMethod.POST)
+	public @ResponseBody
+	VideoStatus setVideoData(@PathVariable(VideoSvcApi.ID_PARAMETER) long id,
+			@RequestParam(VideoSvcApi.DATA_PARAMETER) MultipartFile videoData,
+			HttpServletResponse response) {
+
+			
+			response.setStatus(HttpStatus.NOT_FOUND.value());
+		
+			try {
+				videoDataMgr = VideoFileManager.get();
+				saveSomeVideo(videos.get(id), videoData);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+		return new VideoStatus(VideoStatus.VideoState.READY);
+	}
 	
+	@RequestMapping(value= VideoSvcApi.VIDEO_DATA_PATH, method = RequestMethod.GET)
+	@ResponseBody Response getData(@PathVariable(VideoSvcApi.ID_PARAMETER) long id) {
+		return null;
+	}
+
 	public Video save(Video entity) {
-        checkAndSetId(entity);
-        videos.put(entity.getId(), entity);
-        return entity;
-    }
+		checkAndSetId(entity);
+		videos.put(entity.getId(), entity);
+		return entity;
+	}
 
-    private void checkAndSetId(Video entity) {
-        if(entity.getId() == 0){
-            entity.setId(currentId.incrementAndGet());
-        }
-    }
-	
-	private String getDataUrl(long videoId){
-        String url = getUrlBaseForLocalServer() + "/video/" + videoId + "/data";
-        return url;
-    }
+	private void checkAndSetId(Video entity) {
+		if (entity.getId() == 0) {
+			entity.setId(currentId.incrementAndGet());
+		}
+	}
 
-    private String getUrlBaseForLocalServer() {
-       HttpServletRequest request = 
-           ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
-       String base = 
-          "http://"+request.getServerName() 
-          + ((request.getServerPort() != 80) ? ":"+request.getServerPort() : "");
-       return base;
-    }
-	
+	private String getDataUrl(long videoId) {
+		String url = getUrlBaseForLocalServer() + "/video/" + videoId + "/data";
+		return url;
+	}
+
+	private String getUrlBaseForLocalServer() {
+		HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder
+				.getRequestAttributes()).getRequest();
+		String base = "http://"
+				+ request.getServerName()
+				+ ((request.getServerPort() != 80) ? ":"
+						+ request.getServerPort() : "");
+		return base;
+	}
+
+	public void saveSomeVideo(Video v, MultipartFile videoData)
+			throws IOException {
+		videoDataMgr.saveVideoData(v, videoData.getInputStream());
+	}
 
 }
